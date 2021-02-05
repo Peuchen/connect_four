2462af8915c925537bae040577a56dbf5b878973
peuchen <npeuchen@gmail.com>
2021-02-05 17:37:29 +0100

    Set up a grid using a 2D Array

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,2 +1,19 @@
 class Grid
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new
+    6.times do |row_idx|
+      grid[row_idx] = Array.new
+      7.times do |col_idx|
+        grid[row_idx][col_idx] = "|_|"
+      end
+    end
+  end
+
+  def visualize_grid
+    puts grid.map { |i| i.join }
+    #width = grid.flatten.max.to_s.size + 2
+    #puts grid.map { |pos| pos.map { |i| i.to_s.rjust(width) }.join }
+  end
 end
